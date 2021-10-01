9c4b22cccb1737360f224c917520888bccde544a
admin <admin@DESKTOP-VBQG2JB>
2021-09-30 17:51:45 -0700

    Commit HybridSeleniumCucumber

---
+++ HybridSeleniumCucumber/src/test/java/com/pages/AdminPage.java
@@ -0,0 +1,12 @@
+package com.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.baseClass.Library;
+
+public class AdminPage {
+
+}
